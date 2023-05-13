fe765b26987324fda20fcc82a9af7cb8bfd35ead
Alfendi <tenzosc@uci.edu>
2023-05-13 01:46:00 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+CLIENT_ID = "D_frkm7plOkjQ3xIb6R3MQ"
+CLIENT_SECRET = "MtY7eWYb3lwv97qwi71cSJE48xPE3w"
+REDIRECT_URI = "http://localhost:8080/reddit_callback"
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 by " \
+           "/u/Aveldaheilt'
